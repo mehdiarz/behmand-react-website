4f210e913c86884521f2288f80ef15a2b49a77a5
M.Arezomand <mahdi_arezoomand@yahoo.com>
2025-04-14 16:26:22 +0330

    update layout and styles: add Navbar component, change HTML direction, and include custom font

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <div className=''>
+            <div className="">
+                <img src="./default-logo.png" className="w-64 h-64"></img>
+            </div>
+        </div>
+    )
+  }
+  
+  export default Navbar
